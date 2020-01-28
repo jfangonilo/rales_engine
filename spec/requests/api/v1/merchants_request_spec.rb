2066001245fe733c704f7a04c98c577996eae987
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-28 11:37:18 -0700

    Add merchant show spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,4 +10,14 @@ describe "Merchants API" do
     merchants = JSON.parse(response.body)
     expect(merchants["data"].count).to eq 3
   end
+
+  it "can show a merchant" do
+    db_merchant = create(:merchant)
+
+    get "/api/v1/merchants/#{db_merchant.id}"
+
+    expect(response).to be_successful
+    merchant = JSON.parse(response.body)
+    expect(merchant["data"]["attributes"]["name"]).to eq db_merchant.name
+  end
 end
