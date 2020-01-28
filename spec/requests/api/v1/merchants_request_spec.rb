87d1779b88287a6b19985bc6af3f198c9d93f45e
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-28 16:26:01 -0700

    Add merchant items spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -20,4 +20,20 @@ describe "Merchants API" do
     merchant = JSON.parse(response.body)
     expect(merchant["data"]["attributes"]["name"]).to eq db_merchant.name
   end
+
+  it "can get a merchant's items" do
+    db_merchant = create(:merchant)
+    item_1 = create(:item, merchant: db_merchant)
+    item_2 = create(:item, merchant: db_merchant)
+    item_3 = create(:item, merchant: db_merchant)
+
+    get "/api/v1/merchants/#{db_merchant.id}/items"
+    expect(response).to be_successful
+    items = JSON.parse(response.body)
+
+    expect(items["data"].count).to eq 3
+    items["data"].each do |item|
+      expect(db_merchant.id).to eq item["attributes"]["merchant_id"]
+    end
+  end
 end
