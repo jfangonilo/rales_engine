a743bee0a8e78d7ad5f522b291bdcdd63fd875fa
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-28 18:13:43 -0700

    add merchant invoices route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get 'invoices/index'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index]
+        resources :invoices, only: [:index]
       end
     end
   end
