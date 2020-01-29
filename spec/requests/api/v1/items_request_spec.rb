84a3507bdc3dd5805dd122f968a967221037d2d1
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-28 18:54:24 -0700

    Add item index spec

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "sends a list of items" do
+    merchant = create(:merchant)
+    create_list(:item, 3, merchant: merchant)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+    items = JSON.parse(response.body)
+    expect(items["data"].count).to eq 3
+  end
+
+end
