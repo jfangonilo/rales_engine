3a07526e505550b3b52e8b8e931e84977935a133
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-30 15:58:56 -0700

    Fix model spec fo total_revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -19,13 +19,14 @@ RSpec.describe Merchant, type: :model do
       @item_5 = create(:item, merchant: @merchants[5], unit_price: 100000)
       @item_6 = create(:item, merchant: @merchants[6], unit_price: 1000000)
 
-      @invoice_0 = create(:invoice, customer: @customer, merchant: @merchants[0])
-      @invoice_1 = create(:invoice, customer: @customer, merchant: @merchants[1])
-      @invoice_2 = create(:invoice, customer: @customer, merchant: @merchants[2])
-      @invoice_3 = create(:invoice, customer: @customer, merchant: @merchants[3])
+      @date = "2020-01-01"
+      @invoice_0 = create(:invoice, customer: @customer, merchant: @merchants[0], created_at: @date)
+      @invoice_1 = create(:invoice, customer: @customer, merchant: @merchants[1], created_at: @date)
+      @invoice_2 = create(:invoice, customer: @customer, merchant: @merchants[2], created_at: @date)
+      @invoice_3 = create(:invoice, customer: @customer, merchant: @merchants[3], created_at: @date)
       @invoice_4 = create(:invoice, customer: @customer, merchant: @merchants[4])
-      @invoice_5 = create(:invoice, customer: @customer, merchant: @merchants[5])
-      @invoice_6 = create(:invoice, customer: @customer, merchant: @merchants[6])
+      @invoice_5 = create(:invoice, customer: @customer, merchant: @merchants[5], created_at: @date)
+      @invoice_6 = create(:invoice, customer: @customer, merchant: @merchants[6], created_at: @date)
 
       @invoice_item_0 = create(:invoice_item, item: @item_0, unit_price: @item_0.unit_price, invoice: @invoice_0)
       @invoice_item_1 = create(:invoice_item, item: @item_1, unit_price: @item_1.unit_price, invoice: @invoice_1)
@@ -35,14 +36,13 @@ RSpec.describe Merchant, type: :model do
       @invoice_item_5 = create(:invoice_item, item: @item_5, unit_price: @item_5.unit_price, invoice: @invoice_5)
       @invoice_item_6 = create(:invoice_item, item: @item_6, unit_price: @item_6.unit_price, invoice: @invoice_6)
 
-      @date = "2020-01-01"
-      @transaction_0 = create(:transaction, invoice: @invoice_0, created_at: @date)
-      @transaction_1 = create(:transaction, invoice: @invoice_1, created_at: @date)
-      @transaction_2 = create(:transaction, invoice: @invoice_2, created_at: @date)
-      @transaction_3 = create(:transaction, invoice: @invoice_3, created_at: @date)
+      @transaction_0 = create(:transaction, invoice: @invoice_0)
+      @transaction_1 = create(:transaction, invoice: @invoice_1)
+      @transaction_2 = create(:transaction, invoice: @invoice_2)
+      @transaction_3 = create(:transaction, invoice: @invoice_3)
       @transaction_4 = create(:transaction, invoice: @invoice_4)
-      @transaction_5 = create(:transaction, invoice: @invoice_5, created_at: @date, result: "failed")
-      @transaction_6 = create(:transaction, invoice: @invoice_6, created_at: @date, result: "failed")
+      @transaction_5 = create(:transaction, invoice: @invoice_5, result: "failed")
+      @transaction_6 = create(:transaction, invoice: @invoice_6, result: "failed")
     end
 
     it 'most_revenue' do
@@ -53,7 +53,7 @@ RSpec.describe Merchant, type: :model do
     it 'total_revenue_by_date' do
       result = Merchant.total_revenue(@date)
       expected = 1111
-      expect(result).to eq expected
+      expect(result.total_revenue).to eq expected
     end
   end
 end
