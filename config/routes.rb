ebcdc59bc3f668faa610ce3332a3591f7e19c103
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-29 20:54:40 -0700

    add most revenue route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get "merchants/most_revenue", to: "merchants#show"
       get "merchants/find", to: "merchants#show"
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index]
