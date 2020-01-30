3fd150776b417e641f1a51b298babb15941e0ed9
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-29 20:36:39 -0700

    Add route for single find

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
     namespace :v1 do
+      get "merchants/find", to: "merchants#show"
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index]
         resources :invoices, only: [:index]
@@ -10,5 +12,4 @@ Rails.application.routes.draw do
       resources :items, only: [:index, :show]
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
