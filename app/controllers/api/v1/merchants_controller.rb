7474fc79e599a5a40b93ec56c92b9539b886e1a4
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-29 20:36:49 -0700

    Add actions for single find

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -4,6 +4,14 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def show
-    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+    if params[:id]
+      render json: MerchantSerializer.new(Merchant.find(params[:id]))
+    elsif params[:name]
+      render json: MerchantSerializer.new(Merchant.find_by(name: params[:name]))
+    elsif params[:created_at]
+      render json: MerchantSerializer.new(Merchant.find_by(created_at: params[:created_at]))
+    elsif params[:updated_at]
+      render json: MerchantSerializer.new(Merchant.find_by(updated_at: params[:updated_at]))
+    end
   end
 end
