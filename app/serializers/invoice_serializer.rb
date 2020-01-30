7f8e934d84b389822e6b4020383fc34714e0c97a
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-30 12:10:45 -0700

    Fix serializers

--- app/serializers/invoice_serializer.rb
+++ app/serializers/invoice_serializer.rb
@@ -1,12 +1,12 @@
 class InvoiceSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :status
+  attributes :id, :status, :merchant_id, :customer_id
 
-  attributes :merchant_id do |invoice|
-    invoice.merchant_id
-  end
+  # attributes :merchant_id do |invoice|
+  #   invoice.merchant_id
+  # end
 
-  attributes :customer_id do |invoice|
-    invoice.customer_id
-  end
+  # attributes :customer_id do |invoice|
+  #   invoice.customer_id
+  # end
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,10 +1,6 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description
-
-  attributes :merchant_id do |item|
-    item.merchant_id
-  end
+  attributes :id, :name, :description, :merchant_id
 
   attributes :unit_price do |item|
     item.unit_price.to_s
