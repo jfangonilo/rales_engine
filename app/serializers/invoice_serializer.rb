438f99ad54e589aa27b7bcd1fbd18520c3a0b608
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-30 12:20:21 -0700

    Remove comments

--- app/serializers/invoice_serializer.rb
+++ app/serializers/invoice_serializer.rb
@@ -1,12 +1,4 @@
 class InvoiceSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :status, :merchant_id, :customer_id
-
-  # attributes :merchant_id do |invoice|
-  #   invoice.merchant_id
-  # end
-
-  # attributes :customer_id do |invoice|
-  #   invoice.customer_id
-  # end
 end
