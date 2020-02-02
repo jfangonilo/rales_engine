bcafc9d1056c0688d0877f9b15731021db3a67db
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-01 17:13:48 -0700

    Refactor single find into model

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -4,16 +4,7 @@ class Api::V1::CustomersController < ApplicationController
   end
 
   def show
-    if params[:id]
-      render json: CustomerSerializer.new(Customer.find(params[:id]))
-    elsif params[:first_name]
-      render json: CustomerSerializer.new(Customer.find_by(first_name: params[:first_name]))
-    elsif params[:last_name]
-      render json: CustomerSerializer.new(Customer.find_by(last_name: params[:last_name]))
-    elsif params[:created_at]
-      render json: CustomerSerializer.new(Customer.find_by(created_at: params[:created_at]))
-    elsif params[:updated_at]
-      render json: CustomerSerializer.new(Customer.find_by(updated_at: params[:updated_at]))
-    end
+    customer = Customer.search(params)
+    render json: CustomerSerializer.new(customer)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,18 @@
 class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
+
+  def self.search(params)
+    if params[:id]
+      find(params[:id])
+    elsif params[:first_name]
+      find_by(first_name: params[:first_name])
+    elsif params[:last_name]
+      find_by(last_name: params[:last_name])
+    elsif params[:created_at]
+      find_by(created_at: params[:created_at])
+    elsif params[:updated_at]
+      find_by(updated_at: params[:updated_at])
+    end
+  end
 end
