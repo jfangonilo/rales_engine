13f1b89e894da0969d58c10abbeaa76769d2f4ee
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-01 23:39:51 -0700

    Add customer/merchant routes

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,8 @@ Rails.application.routes.draw do
         resources :transactions, only: [:index]
         resources :invoice_items, only: [:index]
         resources :items, only: [:index]
+        get "/customer", to: "customers#show"
+        get "/merchant", to: "merchants#show"
       end
     end
   end
