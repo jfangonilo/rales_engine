584eeb4273e18ffeceaaf93088a18ef4b8deccb0
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-01 19:53:43 -0700

    Add single finder action

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -7,11 +7,21 @@ class Api::V1::InvoicesController < ApplicationController
     else
       render json: InvoiceSerializer.new(Invoice.all)
     end
+  end
 
-    def show
-      if params[:id]
-        render json: InvoiceSerializer.new(Invoice.find(params[:id]))
-      end
+  def show
+    if params[:id]
+      render json: InvoiceSerializer.new(Invoice.find(params[:id]))
+    elsif params[:customer_id]
+      render json: InvoiceSerializer.new(Invoice.find_by(customer_id: params[:customer_id]))
+    elsif params[:merchant_id]
+      render json: InvoiceSerializer.new(Invoice.find_by(merchant_id: params[:merchant_id]))
+    elsif params[:status]
+      render json: InvoiceSerializer.new(Invoice.find_by(status: params[:status]))
+    elsif params[:created_at]
+      render json: InvoiceSerializer.new(Invoice.find_by(created_at: params[:created_at]))
+    elsif params[:updated_at]
+      render json: InvoiceSerializer.new(Invoice.find_by(updated_at: params[:updated_at]))
     end
   end
 end
