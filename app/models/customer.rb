832a3d362aafb051cd5a603bcd8e63f01556df5e
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-01 17:26:04 -0700

    Add multi finder model methods

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,25 @@ class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
 
+  scope :with_first_name, ->(first_name) { where(first_name: first_name) }
+  scope :with_last_name, ->(last_name) { where(last_name: last_name) }
+
+  def self.search_all(params)
+    if params[:id]
+      with_id(params[:id])
+    elsif params[:first_name]
+      with_first_name(params[:first_name])
+    elsif params[:last_name]
+      with_last_name(params[:last_name])
+    elsif params[:created_at]
+      created_on(params[:created_at])
+    elsif params[:updated_at]
+      updated_on(params[:updated_at])
+    else
+      all
+    end
+  end
+
   def self.search(params)
     if params[:id]
       find(params[:id])
