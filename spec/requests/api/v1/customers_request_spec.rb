03fcb781c997cb0f3c7b73567755b1e6640316e8
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-02 14:02:20 -0700

    Add favorite merchant spec

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -199,4 +199,37 @@ describe "Customers API" do
 
     expect(parsed_transactions.count).to eq 3
   end
+
+  it "can get favorite merchant for a customer" do
+    customer = create(:customer)
+
+    merchant_0 = create(:merchant)
+    invoice_0 = create(:invoice, customer: customer, merchant: merchant_0)
+    transaction_0 = create(:transaction, invoice: invoice_0, result: "success")
+    transaction_1 = create(:transaction, invoice: invoice_0, result: "success")
+    transaction_2 = create(:transaction, invoice: invoice_0, result: "success")
+    transaction_3 = create(:transaction, invoice: invoice_0, result: "success")
+    transaction_4 = create(:transaction, invoice: invoice_0, result: "failed")
+
+    merchant_1 = create(:merchant)
+    invoice_1 = create(:invoice, customer: customer, merchant: merchant_1)
+    transaction_4 = create(:transaction, invoice: invoice_1, result: "success")
+    transaction_5 = create(:transaction, invoice: invoice_1, result: "success")
+    transaction_6 = create(:transaction, invoice: invoice_1, result: "failed")
+    transaction_7 = create(:transaction, invoice: invoice_1, result: "failed")
+    transaction_8 = create(:transaction, invoice: invoice_1, result: "failed")
+    transaction_9 = create(:transaction, invoice: invoice_1, result: "failed")
+
+    merchant_2 = create(:merchant)
+    invoice_2 = create(:invoice, customer: customer, merchant: merchant_2)
+    transaction_10 = create(:transaction, invoice: invoice_2)
+    transaction_11 = create(:transaction, invoice: invoice_2)
+    transaction_12 = create(:transaction, invoice: invoice_2)
+
+    get "/api/v1/customers/#{customer.id}/favorite_merchant"
+    expect(response).to be_successful
+    result = JSON.parse(response.body)["data"]
+    expect(result["attributes"]["id"]).to eq merchant_0.id
+    expect(result["attributes"]["name"]).to eq merchant_0.name
+  end
 end
