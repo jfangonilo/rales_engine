9cb9fb6e2ee14ac65afd635a77d8beae26eb7019
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-01 20:38:53 -0700

    Move search methods to model

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,35 +1,11 @@
 class Api::V1::InvoicesController < ApplicationController
   def index
-    if params[:id]
-      render json: InvoiceSerializer.new(Invoice.with_id(params[:id]))
-    elsif params[:status]
-      render json: InvoiceSerializer.new(Invoice.with_status(params[:status]))
-    elsif params[:created_at]
-      render json: InvoiceSerializer.new(Invoice.created_on(params[:created_at]))
-    elsif params[:updated_at]
-      render json: InvoiceSerializer.new(Invoice.updated_on(params[:updated_at]))
-    elsif params[:merchant_id]
-      render json: InvoiceSerializer.new(Invoice.from_merchant(params[:merchant_id]))
-    elsif params[:customer_id]
-      render json: InvoiceSerializer.new(Invoice.from_customer(params[:customer_id]))
-    else
-      render json: InvoiceSerializer.new(Invoice.all)
-    end
+    invoices = Invoice.search_all(params)
+    render json: InvoiceSerializer.new(invoices)
   end
 
   def show
-    if params[:id]
-      render json: InvoiceSerializer.new(Invoice.find(params[:id]))
-    elsif params[:customer_id]
-      render json: InvoiceSerializer.new(Invoice.find_by(customer_id: params[:customer_id]))
-    elsif params[:merchant_id]
-      render json: InvoiceSerializer.new(Invoice.find_by(merchant_id: params[:merchant_id]))
-    elsif params[:status]
-      render json: InvoiceSerializer.new(Invoice.find_by(status: params[:status]))
-    elsif params[:created_at]
-      render json: InvoiceSerializer.new(Invoice.find_by(created_at: params[:created_at]))
-    elsif params[:updated_at]
-      render json: InvoiceSerializer.new(Invoice.find_by(updated_at: params[:updated_at]))
-    end
+    invoice = Invoice.search(params)
+    render json: InvoiceSerializer.new(invoice)
   end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,4 +8,38 @@ class Invoice < ApplicationRecord
   scope :from_merchant, ->(merchant_id) { where(merchant_id: merchant_id) }
   scope :from_customer, ->(customer_id) { where(customer_id: customer_id) }
   scope :with_status, ->(status) { where(status: status) }
+
+  def self.search_all(params)
+    if params[:id]
+      with_id(params[:id])
+    elsif params[:status]
+      with_status(params[:status])
+    elsif params[:created_at]
+      created_on(params[:created_at])
+    elsif params[:updated_at]
+      updated_on(params[:updated_at])
+    elsif params[:merchant_id]
+      from_merchant(params[:merchant_id])
+    elsif params[:customer_id]
+      from_customer(params[:customer_id])
+    else
+      all
+    end
+  end
+
+  def self.search(params)
+    if params[:id]
+      find(params[:id])
+    elsif params[:customer_id]
+      find_by(customer_id: params[:customer_id])
+    elsif params[:merchant_id]
+      find_by(merchant_id: params[:merchant_id])
+    elsif params[:status]
+      find_by(status: params[:status])
+    elsif params[:created_at]
+      find_by(created_at: params[:created_at])
+    elsif params[:updated_at]
+      find_by(updated_at: params[:updated_at])
+    end
+  end
 end
