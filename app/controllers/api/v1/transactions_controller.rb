9ad1dfc1dea61be8e34f8fac17328dd985efa267
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-01 18:50:13 -0700

    Refactor customer transactions search

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::TransactionsController < ApplicationController
   def index
     if params[:customer_id]
-      customer = Customer.find(params[:customer_id])
-      render json: TransactionSerializer.new(customer.transactions)
+      transactions = Transaction.belonging_to_customer(params[:customer_id])
+      render json: TransactionSerializer.new(transactions)
     end
   end
 end
