7a2c7b4dfc7911b126d80addb719a90503c84a5b
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-01 20:29:12 -0700

    Add find_all route

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
       end
 
       get "invoices/find", to: "invoices#show"
+      get "invoices/find_all", to: "invoices#index"
       resources :invoices, only: [:index, :show]
     end
   end
