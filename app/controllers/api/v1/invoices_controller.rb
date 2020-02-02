d81bbe04885bc84bf767c4900633af9d57d8cd88
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-01 19:00:26 -0700

    Add invoices index and show action

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -4,6 +4,14 @@ class Api::V1::InvoicesController < ApplicationController
       render json: InvoiceSerializer.new(Invoice.from_merchant(params[:merchant_id]))
     elsif params[:customer_id]
       render json: InvoiceSerializer.new(Invoice.from_customer(params[:customer_id]))
+    else
+      render json: InvoiceSerializer.new(Invoice.all)
+    end
+
+    def show
+      if params[:id]
+        render json: InvoiceSerializer.new(Invoice.find(params[:id]))
+      end
     end
   end
 end
