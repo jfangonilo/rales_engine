23fcad3e54f04690ea483393461d00d4cb4b3c96
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-01 17:05:30 -0700

    Add action for single finders

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -4,6 +4,12 @@ class Api::V1::CustomersController < ApplicationController
   end
 
   def show
-    render json: CustomerSerializer.new(Customer.find(params[:id]))
+    if params[:id]
+      render json: CustomerSerializer.new(Customer.find(params[:id]))
+    elsif params[:first_name]
+      render json: CustomerSerializer.new(Customer.find_by(first_name: params[:first_name]))
+    elsif params[:last_name]
+      render json: CustomerSerializer.new(Customer.find_by(last_name: params[:last_name]))
+    end
   end
 end
