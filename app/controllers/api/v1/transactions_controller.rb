230768c41a2020b7325942dc3e3a247c279da84c
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-02 19:39:44 -0700

    Move search to model

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -11,19 +11,7 @@ class Api::V1::TransactionsController < ApplicationController
   end
 
   def show
-    if params[:id]
-      transaction = Transaction.find(params[:id])
-    elsif params[:invoice_id]
-      transaction = Transaction.find_by(invoice_id: params[:invoice_id])
-    elsif params[:credit_card_number]
-      transaction = Transaction.find_by(credit_card_number: params[:credit_card_number])
-    elsif params[:result]
-      transaction = Transaction.find_by(result: params[:result])
-    elsif params[:created_at]
-      transaction = Transaction.find_by(created_at: params[:created_at])
-    elsif params[:updated_at]
-      transaction = Transaction.find_by(updated_at: params[:updated_at])
-    end
+    transaction = Transaction.search(params)
     render json: TransactionSerializer.new(transaction)
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -8,4 +8,19 @@ class Transaction < ApplicationRecord
     where("invoices.customer_id = ?", customer_id)
   }
 
+  def self.search(params)
+    if params[:id]
+      find(params[:id])
+    elsif params[:invoice_id]
+      find_by(invoice_id: params[:invoice_id])
+    elsif params[:credit_card_number]
+      find_by(credit_card_number: params[:credit_card_number])
+    elsif params[:result]
+      find_by(result: params[:result])
+    elsif params[:created_at]
+      find_by(created_at: params[:created_at])
+    elsif params[:updated_at]
+      find_by(updated_at: params[:updated_at])
+    end
+  end
 end
