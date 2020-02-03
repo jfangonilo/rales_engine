7b9f45fccba6adb08660f0953fdaf58b7fb8f22f
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-02 19:50:16 -0700

    Add  multi finder actions

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::TransactionsController < ApplicationController
   def index
-    if params[:customer_id]
+    if params[:id]
+      transactions = Transaction.with_id(params[:id])
+    elsif params[:credit_card_number]
+      transactions = Transaction.with_credit_card_number(params[:credit_card_number])
+    elsif params[:result]
+      transactions = Transaction.with_result(params[:result])
+    elsif params[:created_at]
+      transactions = Transaction.created_on(params[:created_at])
+    elsif params[:updated_at]
+      transactions = Transaction.updated_on(params[:updated_at])
+    elsif params[:customer_id]
       transactions = Transaction.from_customer(params[:customer_id])
     elsif params[:invoice_id]
       transactions = Transaction.from_invoice(params[:invoice_id])
