9fc6cd8b5e71fcdc98f53617d90c2f9ad0cbda6d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-02 19:37:22 -0700

    Add single finder actions

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -13,6 +13,16 @@ class Api::V1::TransactionsController < ApplicationController
   def show
     if params[:id]
       transaction = Transaction.find(params[:id])
+    elsif params[:invoice_id]
+      transaction = Transaction.find_by(invoice_id: params[:invoice_id])
+    elsif params[:credit_card_number]
+      transaction = Transaction.find_by(credit_card_number: params[:credit_card_number])
+    elsif params[:result]
+      transaction = Transaction.find_by(result: params[:result])
+    elsif params[:created_at]
+      transaction = Transaction.find_by(created_at: params[:created_at])
+    elsif params[:updated_at]
+      transaction = Transaction.find_by(updated_at: params[:updated_at])
     end
     render json: TransactionSerializer.new(transaction)
   end
