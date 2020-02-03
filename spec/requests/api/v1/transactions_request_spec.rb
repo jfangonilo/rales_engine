ede8fc9456bb08b8996db81937af9b7449c2da90
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-02 19:49:59 -0700

    Add multi finder spec

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -110,4 +110,78 @@ describe "Transactions API" do
     result = JSON.parse(response.body)["data"]
     expect(result["attributes"]["id"]).to eq transaction.id
   end
+
+  it "can get a list of transactions by id" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+    transaction = create(:transaction, invoice: invoice)
+
+    get "/api/v1/transactions/find_all?id=#{transaction.id}"
+    expect(response).to be_successful
+    result = JSON.parse(response.body)["data"]
+    expect(result.first["attributes"]["id"]).to eq transaction.id
+  end
+
+  it "can get a list of transactions by invoice_id" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+    transaction = create(:transaction, invoice: invoice)
+
+    get "/api/v1/transactions/find_all?invoice_id=#{transaction.invoice_id}"
+    expect(response).to be_successful
+    result = JSON.parse(response.body)["data"]
+    expect(result.first["attributes"]["id"]).to eq transaction.id
+  end
+
+  it "can get a list of transactions by credit_card_number" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+    transaction = create(:transaction, invoice: invoice)
+
+    get "/api/v1/transactions/find_all?credit_card_number=#{transaction.credit_card_number}"
+    expect(response).to be_successful
+    result = JSON.parse(response.body)["data"]
+    expect(result.first["attributes"]["id"]).to eq transaction.id
+  end
+
+  it "can get a list of transactions by result" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+    transaction = create(:transaction, invoice: invoice)
+
+    get "/api/v1/transactions/find_all?result=#{transaction.result}"
+    expect(response).to be_successful
+    result = JSON.parse(response.body)["data"]
+    expect(result.first["attributes"]["id"]).to eq transaction.id
+  end
+
+  it "can get a list of transactions by created_at" do
+    date = "2012-01-01"
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+    transaction = create(:transaction, invoice: invoice, created_at: date)
+
+    get "/api/v1/transactions/find_all?created_at=#{transaction.created_at}"
+    expect(response).to be_successful
+    result = JSON.parse(response.body)["data"]
+    expect(result.first["attributes"]["id"]).to eq transaction.id
+  end
+
+  it "can get a list of transactions by updated_at" do
+    date = "2012-01-01"
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+    transaction = create(:transaction, invoice: invoice, updated_at: date)
+
+    get "/api/v1/transactions/find_all?updated_at=#{transaction.updated_at}"
+    expect(response).to be_successful
+    result = JSON.parse(response.body)["data"]
+    expect(result.first["attributes"]["id"]).to eq transaction.id
+  end
 end
